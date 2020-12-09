eaf203a7cb7c09d54784ee56f9e65febfd876023
Greg Bowler <greg.bowler@g105b.com>
2020-12-09 12:11:52 +0000

    Remove repo name from tag

--- setup-php.bash
+++ setup-php.bash
@@ -17,7 +17,7 @@ ADD https://raw.githubusercontent.com/mlocati/docker-php-extension-installer/mas
 RUN chmod +x /usr/local/bin/install-php-extensions && sync && install-php-extensions"
 fi
 
-dockerfile_unique="${ACTION_PHP_VERSION}-${GITHUB_REPOSITORY}"
+dockerfile_unique="${ACTION_PHP_VERSION}"
 for ext in $ACTION_PHP_EXTENSIONS
 do
 	dockerfile="${dockerfile} $ext"
